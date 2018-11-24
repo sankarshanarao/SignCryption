aa882a6d01d9713079be6c83371aa0f6e15005e4
rohanmohapatra <rohanironcrest14@gmail.com>
2018-11-24 15:04:38 +0530

    Added MD5

--- src/SignCryption.java
+++ src/SignCryption.java
@@ -1,9 +1,13 @@
 import java.math.*;
 import java.util.*;
 import java.util.LinkedList;
+import java.math.BigInteger; 
+import java.security.MessageDigest; 
+import java.security.NoSuchAlgorithmException; 
 
 public class SignCryption {
     private BigInteger p, q, g, x, y, yo, shSc;
+    private String k,k1,k2;
 
 	// // Constructors // // 
 
@@ -85,7 +89,34 @@ public class SignCryption {
 	}
 
 	// // // // // // // // Logic // // // // // // // //
-
+	public static String getMd5(String input) 
+    { 
+        try { 
+  
+            // Static getInstance method is called with hashing MD5 
+            MessageDigest md = MessageDigest.getInstance("MD5"); 
+  
+            // digest() method is called to calculate message digest 
+            //  of an input digest() return array of byte 
+            byte[] messageDigest = md.digest(input.getBytes()); 
+  
+            // Convert byte array into signum representation 
+            BigInteger no = new BigInteger(1, messageDigest); 
+  
+            // Convert message digest into hex value 
+            String hashtext = no.toString(16); 
+            while (hashtext.length() < 32) { 
+                hashtext = "0" + hashtext; 
+            } 
+            return hashtext; 
+        }  
+  
+        // For specifying wrong message digest algorithms 
+        catch (NoSuchAlgorithmException e) { 
+            throw new RuntimeException(e); 
+        } 
+    } 
+	
     private static Boolean isSpsp(BigInteger n, BigInteger a) {
 		BigInteger two = BigInteger.valueOf(2);
 		BigInteger n1 = n.subtract(BigInteger.ONE);
